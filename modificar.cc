3a472464b27e97a69a2aa79059c7a760eb3b6174
sergiorvs <segio.rolando@gmail.com>
2016-09-17 17:49:37 -0500

    nueva linea + sumarf

--- modificar.cc
+++ modificar.cc
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+int sumar(int a,int b)
+{
+    return a+b;
+}
+
 int main()
 {
     cout<<"HELLO WORD!"<<endl;
+
+    cout<<"nueva modificacion!!!"<<endl;
+    cout<<sumar(5,7)<<endl;
     return 0;
 }

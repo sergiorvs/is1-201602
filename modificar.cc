006e12b5ca76dfc8f33d4c87bee147a0395e3877
sergiorvs <segio.rolando@gmail.com>
2016-09-21 17:38:48 -0500

    ss

--- modificar.cc
+++ modificar.cc
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-<<<<<<< HEAD
 int sumar(int a,int b)
 {
     return a+b;
@@ -14,16 +13,11 @@ int main()
 
     cout<<"nueva modificacion!!!"<<endl;
     cout<<sumar(5,7)<<endl;
-=======
-//first modification in new-branch
-int main()
-{
     cout<<"HELLO WORD!"<<endl;
     cout<<"Si hacemos el parse"<<endl;
     cout<<"first modification in new-branch"<<endl;
     cout<<"hola :)"<<endl;
     cout<<"second modification in new-branch"<<endl;
     cout<<"generanando conflictos ^^" << endl;
->>>>>>> ff4d10f8ac676ab74a52b40cbc83e73487f88958
     return 0;
 }

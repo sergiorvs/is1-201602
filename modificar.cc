4f53cc6ed4f015cbc7ba6e9d9dd0467b5205cd02
ltch23 <luiston23@gmail.com>
2016-09-21 09:36:29 -0500

    adding a new function 'salute'

--- modificar.cc
+++ modificar.cc
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <string>
+
 
 using namespace std;
 
 //first modification in new-branch
+void salute();
 int main()
 {
     cout<<"HELLO WORD!"<<endl;
     /*translating*/
     cout<<"Yes, we do the parse"<<endl;/**/
     cout<<"first modification in new-branch"<<endl;
+
+
+    salute();	
     return 0;
+}
+
+void salute()
+{
+	string str="";
+	cout<<"get into his name: ";
+	cin>>str;
+	std::cout<<"\nHola: "<<str<<std::endl;
 }
